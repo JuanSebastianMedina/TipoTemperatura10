b7f3378b7ae4d45fe9d02b9bf6151b75f0f92c3e
Juanse <jmedinapineros@gmail.com>
2025-09-06 00:26:59 -0500

    El programa cumple con los requerimientos

--- src/tipotemperatura10/TipoTemperatura10.java
+++ src/tipotemperatura10/TipoTemperatura10.java
@@ -3,18 +3,28 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package tipotemperatura10;
-
 /**
- *
  * @author juanse
+ * fecha: viernes 05 de septiembre de 2025
+ * Requerimientos: Ingresar una temperatura y clasificarlo como muy fria, fria, agradable, calurosa o muy calusora
  */
+import javax.swing.JOptionPane;
 public class TipoTemperatura10 {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int grados = Integer.parseInt(JOptionPane.showInputDialog("Ingrese una temperatura"));
+        if (grados>50) {
+            JOptionPane.showMessageDialog(null, "Temperatura muy calusosa");
+        } else if (grados>30) {
+            JOptionPane.showMessageDialog(null, "Temperatura calurosa");
+        } else if (grados>15) {
+            JOptionPane.showMessageDialog(null, "Temperatura agradable");
+        } else if (grados > -5) {
+            JOptionPane.showMessageDialog(null, "Temperatura fria");
+        } else {
+            JOptionPane.showMessageDialog(null, "Temperatura muy fria");
+        }
     }
-    
 }
